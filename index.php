01e5c2e2456097b7c29364fc25d6abac43e9fae7
Yoan <pro@ysavary.fr>
2020-06-04 20:26:52 +0200

    main

---
+++ index.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Sivahera coffee Tradeing and consulting firm specializing in management">
+  <meta name="author" content="Com'On Réal http://comon-real.fr">
+  <title>Sivahera</title>
+
+
+  <!-- Bootstrap core CSS -->
+  <link href="bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+
+  <meta name="theme-color" content="#563d7c">
+
+
+  <style>
+    .bd-placeholder-img {
+      font-size: 1.125rem;
+      text-anchor: middle;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+    }
+
+    @media (min-width: 768px) {
+      .bd-placeholder-img-lg {
+        font-size: 3.5rem;
+      }
+    }
+
+    .bg {
+      margin: 0;
+      padding: 0;
+      background: url(./background.jpg) no-repeat center fixed;
+      -webkit-background-size: cover;
+      /* pour anciens Chrome et Safari */
+      background-size: cover;
+      /* version standardisée */
+    }
+
+    .masthead-brand {
+      float: left;
+      padding: 15px;
+      text-align: center;
+      width: 100%;
+    }
+
+    .masthead-brand {
+      float: none;
+    }
+
+    @supports ((-webkit-backdrop-filter: none) or (backdrop-filter: none)) {
+      .cover {
+        -webkit-backdrop-filter: blur(5%);
+        backdrop-filter: blur(5%);
+        background-color: rgba(255, 255, 255, 0.1);
+      }
+    }
+  </style>
+  <!-- Custom styles for this template -->
+  <link href="cover.css" rel="stylesheet">
+</head>
+
+<body class="text-center bg">
+  <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <header class="masthead mb-auto">
+      <div class="inner">
+        <h3 class="masthead-brand">Sivahera</h3>
+      </div>
+    </header>
+
+    <main role="main" class="inner cover">
+      <h1 class="cover-heading">Sivahera</h1>
+      <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the
+        text, and add your own fullscreen background photo to make it your own.</p>
+      <p class="lead">
+        <a href="https://cafe.sivahera.com/" class="btn btn-lg btn-secondary">Café</a>
+        <a href="https://management.sivahera.com/" class="btn btn-lg btn-secondary">Management</a>
+      </p>
+    </main>
+
+    <footer class="mastfoot mt-auto">
+      <div class="inner">
+        <p>Site fait par <a href="https://redirict.comon-real.fr">Com'On Réal</a></p>
+      </div>
+    </footer>
+  </div>
+</body>
+
+</html>
